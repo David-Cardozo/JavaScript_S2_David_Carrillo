b41858e8774241cc5ad5122b59d34304245812a7
DavidCarrillo <MiniMrSanty@gmail.com>
2025-07-18 12:23:54 -0500

    Feat: :sparkles: my first commit

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,54 @@
+//############################################
+//############ Dia 1 #########################
+//############################################
+
+//¡¡Primer comando!!
+console.log("Hola Mundo")
+
+variable1 = 3.14;
+
+console.log(variable1);
+
+console.log(typeof variable1);
+
+//Concatenacion
+
+palabra1="Campus";
+
+palabra2="Lands";
+
+palabra3=palabra1+ palabra2;
+
+console.log(palabra3);
+
+//Booleano
+
+booleanito1 = true;
+
+booleanito2 = false;
+
+console.log(typeof booleanito1);
+
+//Condicionales
+
+if(booleanito1 == true){
+    console.log("Es Verdadero!");
+}
+else{
+    console.log("No es verdadero");
+}
+
+//Bucles
+
+for(i=0;i<4;i++){
+    console.log("Wenas");
+}
+
+holi = true;
+
+while(holi==true){
+    console.log("Wenass");
+    holi=false;
+}
+
+// Desarrollado por: David Carrillo
